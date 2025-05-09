84b987da50c4b99982776d617866c49fa37a7470
Aday <adayperdomorodriguez@gmail.com>
2025-05-09 10:44:06 +0100

    mensajes

--- scipt.js
+++ scipt.js
@@ -1,37 +1,60 @@
 "use strict"
 
+let ciudadesBuscadas = [];
+
 document.getElementById("boton").addEventListener("click", function(e){
     e.preventDefault();
     consultarAPI();
 });
 
-
 function consultarAPI(){
     let xhr, city, apiKey, url;
+    let pError = document.getElementById("mensaje-error");
 
-    city = document.getElementById("input").value;
-    apiKey = "6dc317711b99db905e1bb85823bc495c"
+    city = document.getElementById("input").value.trim();
+
+    if(city === ""){
+        pError.textContent = "Por favor, escribe una ciudad";
+        pError.className = "error activo";
+        return;
+    }
+
+    if(ciudadesBuscadas.includes(city.toLowerCase())){
+        pError.textContent = "Ya has buscado esta ciudad";
+        pError.className = "error advertencia";
+        return;
+    }
+
+    apiKey = "6dc317711b99db905e1bb85823bc495c";
     url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+
     xhr = new XMLHttpRequest();
     xhr.onload = function(){
-        mostrarInformacion(this);
+        mostrarInformacion(this, city);
     }
     xhr.open("GET", url);
     xhr.send();
-
 }
 
-function mostrarInformacion(xhr){
+function mostrarInformacion(xhr, city){
+    let pError = document.getElementById("mensaje-error");
+
+    if(xhr.status !== 200){
+        pError.textContent = "Ciudad no encontrada. Inténtalo de nuevo";
+        pError.className = "error activo";
+        return;
+    }
+
     let obj = JSON.parse(xhr.responseText);
 
-    let city = obj.name;
+    let cityName = obj.name;
     let temp = String(Math.round(obj.main.temp));
     let weatherDescription = obj.weather[0].description;
     let country = obj.sys.country;
     let icon = "https://openweathermap.org/img/wn/" + obj.weather[0].icon + "@2x.png";
 
     let codigoHTML ="<div class='prueba'>" +
-                        "<p class='nombre'>"+ city + "<sup>"+ country + "</sup></p>" +
+                        "<p class='nombre'>"+ cityName + "<sup>"+ country + "</sup></p>" +
                         "<p class='temperatura'>"+ temp + "<sup>ºC</sup></p>" +
                         "<figure>" +
                             "<img src='" + icon + "'>" +
@@ -41,5 +64,9 @@ function mostrarInformacion(xhr){
 
     document.getElementById("tarjetas").innerHTML += codigoHTML;
 
+    ciudadesBuscadas.push(city.toLowerCase());
+
     document.getElementById("input").value = "";
+
+    pError.className = "error";
 }
